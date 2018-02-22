cc80c1f3f01120b62555422a22dc246b7cd991fa
Szymon Malewski <malewskiszymon@gmail.com>
2018-02-22 18:57:36 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var a = prompt('Enter a'),
 	h = prompt('Enter h'),
 	b = prompt('Enter b'),
-	triangleArea = a*h/2;
+	triangleArea = a*h/2,
 	math = (a * a) - (2 * a * b) - (b * b);
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
@@ -9,12 +9,12 @@ console.log(math);
 
 if (math > 0) {
 	console.log("Wynik dodatni");
-}
-else if (math == 0) {
+}else if (math === 0) {
 	console.log("Wynik to 0");
-}
-else {
+}else if (math < 0) {
 	console.log("Wynik ujemny");
+}else {
+	console.log("Miała być liczba!!");
 }
 
 
